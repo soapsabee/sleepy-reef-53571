7da0c5e99e8815dca0dfbd9e721c3a2bc971db90
soapsabee <s5930213055@phuket.psu.ac.th>
2018-11-05 20:30:42 +0700

    make it happy

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cors())
 // index page
-
+app.get('/', function (req, res) {
+    res.send('Express is running');
+    });
 
 app.get('/api/products', db.getAllProducts);
 app.get('/api/products/:id', db.getProductByID);
